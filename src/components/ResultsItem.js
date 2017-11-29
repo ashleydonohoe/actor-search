bba6ad2aa8a8dbe196d0dff3f3cf3a8d85b06a8c
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-11-28 23:51:35 -0500

    working on movie list

--- src/components/ResultsItem.js
+++ src/components/ResultsItem.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-export default(props) => {
-
+export default({person}) => {
+  const knownFor = person.known_for;
+  const name = person.name;
+  const imageURL = person.profile_path ? `https://image.tmdb.org/t/p/w320/${person.profile_path}` : 'http://via.placeholder.com/320x480?text=No%20Image%20Available';
+  const knownForList = knownFor.map((item) => {
+    return (
+      <li key={item.id}>{item.title} - {item.vote_average}/10</li>
+    );
+  });
+  console.log(knownForList);
   return (
-    <div>One Result</div>
-  );
+  <div className="col-md-4 col-sm-6 text-center actor-cell" key={name}>
+    <h3>{name}</h3>
+    <img src={imageURL} alt={name} className="img-thumbnail"/>
+    <h4>Top Shows/Movies</h4>
+    <ul>{knownForList}</ul>
+  </div>
+ );
 }
